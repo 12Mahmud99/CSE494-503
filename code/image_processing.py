069f31182bef37e94b258406d06512bf9b42c69f
mea <mahmuodimad@gmail.com>
2025-08-25 23:18:14 -0700

    updated .py

---
+++ code/image_processing.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+ 
+img = cv.imread('image.webp', cv.IMREAD_GRAYSCALE)
+assert img is not None, "file could not be read, check with os.path.exists()"
+ret,thresh1 = cv.threshold(img,127,255,cv.THRESH_BINARY)
+
+ 
+titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+images = [img, thresh1]
+ 
+for i in range(len(images)):
+    plt.plot(2,3,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+    plt.title(titles[i])
+    plt.xticks([]),plt.yticks([])
+ 
+plt.show()
